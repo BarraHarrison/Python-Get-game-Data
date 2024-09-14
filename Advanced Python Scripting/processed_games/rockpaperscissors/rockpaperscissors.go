7f6537cb4224bb3a44c9814466938222b0681afb
Barra Harrison <143306095+BarraHarrison@users.noreply.github.com>
2024-09-14 15:47:06 +0900

    Add files via upload

---
+++ Advanced Python Scripting/processed_games/rockpaperscissors/rockpaperscissors.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Welcome to Rock-Paper-Scissors!")
+    // Add your game logic here
+}
---
+++ Advanced Python Scripting/processed_games/wordsperminute/wordsperminute.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Welcome to Words Per Minute!")
+    // Add your game logic here
+}
